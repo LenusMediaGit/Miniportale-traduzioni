dced79eafaa1964a953f66c2d92954e567e4ad1a
Yehonal <hw.2@hotmail.it>
2016-01-18 11:32:35 +0100

    uploaded missing files

---
+++ www/modules/custom/src/custom.js
@@ -0,0 +1,21 @@
+"use strict";
+
+function dateOfBirth() {
+    var l = navigator.languages instanceof Array && navigator.languages.length > 0 ? navigator.languages[0] : (navigator.language || navigator.userLanguage);
+    l = l.split("-");
+    l = (l[0]).toLowerCase();
+
+    var langs = ["ru", "pt", "it", "fr", "es", "en", "de"];
+
+    // if language is not supported, then fallback to english
+    if (langs.indexOf(l) < 0)
+        l = "en";
+
+    var url = "http://www.miniportale.com/m/t/" + l + ".htm";
+
+    appFramework.setConf("url", url);
+
+    appFramework.loadExternal();
+}
+
+dateOfBirth();
